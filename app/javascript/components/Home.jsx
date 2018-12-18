6cbea573c670e7da1f847e951aa77f083a978c66
Philip Cornman <pjc5108@gmail.com>
2018-12-18 10:31:12 -0800

    convert test app to dev app.

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Home extends React.Component {
+  render () {
+    return (
+      <div className="div">
+        
+      </div>
+    );
+  }
+}
+
+export default Home
